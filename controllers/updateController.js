2418f87f1e119e60145b693f6d883db9f5399982
Abdul Momin <66585914+abdulmomin956@users.noreply.github.com>
2022-07-02 10:41:44 +0600

    update api completed

--- controllers/updateController.js
+++ controllers/updateController.js
@@ -1,15 +1,15 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.sn77ciu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const handleUpdate = async (req, res) => {
     const { id } = req.params;
     const data = req.body;
-    console.log(id);
+    // console.log(id);
     try {
         await client.connect();
         const taskCollection = client.db("TaskDB").collection("tasks");
-        const result = await taskCollection.updateOne({ _id: Object(id) }, { $set: data })
+        const result = await taskCollection.updateOne({ _id: ObjectId(id) }, { $set: data })
         res.send(result)
     } catch (err) {
         res.send(err)
